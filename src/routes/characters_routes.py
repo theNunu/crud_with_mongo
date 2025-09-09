29534bcf4ef079224f9d78ef6ffb94332e4ca120
theNunu <152096019+theNunu@users.noreply.github.com>
2025-09-09 11:51:47 -0500

    fix el obtener

--- src/routes/characters_routes.py
+++ src/routes/characters_routes.py
@@ -15,6 +15,16 @@ app = FastAPI(
 def read_root():
     return {"message": "un video mas mi gente pa perder el tiempo"}
 
+# Serializador para convertir documentos de MongoDB a JSON
+def character_serializer(character) -> dict:
+    return {
+        "message": "campeon obtenido: ",
+        "id personaje": str(character["_id"]), #Convierte el ObjectId en una cadena (str(champion["_id"])).
+        "name personaje": character["name"],
+        "last_name personaje": character["last_name"],
+        "age personaje": character["age"]
+    }
+
 @app.post("/guardar_character")
 def guardar_personaje(data: Character):
     character = iniciar_conexion()
@@ -38,3 +48,21 @@ def guardar_personaje(data: Character):
         "año": data.age,
         "fecha_creacion": tiempo_generado
     }
+
+@app.get("/obtener_todos_personajes")
+def traer_todos_personajes():
+    characters = iniciar_conexion()
+    
+    # def iniciar_bucle():
+    #     for c in character.find():
+    #         # print(c)
+    #         return c
+    # result = iniciar_bucle()
+    # print(f"resultado del bucle: {result}")
+    personajes = [character_serializer(char) for char in characters.find()] 
+    print(f"\nall characters: {personajes}")
+    # return {"message": "se trajo todos los personajes!!!",
+    #         # "bucle": result
+        
+    # }
+    return personajes
