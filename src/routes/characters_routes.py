571c9ef6238ac348aae82620accff289caf06475
theNunu <152096019+theNunu@users.noreply.github.com>
2025-09-18 10:13:39 -0500

    arreglado el obtener

--- src/routes/characters_routes.py
+++ src/routes/characters_routes.py
@@ -10,19 +10,20 @@ app = FastAPI(
 
 )
 
-
 @app.get("/")
 def read_root():
     return {"message": "un video mas mi gente pa perder el tiempo"}
 
 # Serializador para convertir documentos de MongoDB a JSON
 def character_serializer(character) -> dict:
+    tiempo_generado = generar_hora_y_fecha()
     return {
         "message": "campeon obtenido: ",
         "id personaje": str(character["_id"]), #Convierte el ObjectId en una cadena (str(champion["_id"])).
-        "name personaje": character["name"],
-        "last_name personaje": character["last_name"],
-        "age personaje": character["age"]
+        "name personaje": character["nombre"],
+        "last_name personaje": character["apellido"],
+        "age personaje": character["año"],
+        "se obtuvo en": tiempo_generado
     }
 
 @app.post("/guardar_character")
@@ -40,7 +41,8 @@ def guardar_personaje(data: Character):
     
     character.insert_one(informacion)
     
-    
+    #en este return se basa los nombres para llamarlos en otras operaciones
+    # LOQ QUE ESTA DENTRO " "
     return {
         "message": "se guardo el personaje con exito!!!",
         "nombre": data.name,
@@ -52,7 +54,6 @@ def guardar_personaje(data: Character):
 @app.get("/obtener_todos_personajes")
 def traer_todos_personajes():
     characters = iniciar_conexion()
-    
     # def iniciar_bucle():
     #     for c in character.find():
     #         # print(c)
@@ -61,8 +62,29 @@ def traer_todos_personajes():
     # print(f"resultado del bucle: {result}")
     personajes = [character_serializer(char) for char in characters.find()] 
     print(f"\nall characters: {personajes}")
-    # return {"message": "se trajo todos los personajes!!!",
-    #         # "bucle": result
-        
-    # }
     return personajes
+
+@app.get("/find_by_name")
+def encontrar_por_nombre():
+    characters = iniciar_conexion()
+    
+    personajes = [character_serializer(c) for c in characters.find({"nombre": "string" })]
+    return personajes
+
+
+@app.put("/editar_personaje/{id_personaje}")
+def edit_character(id_personaje: str, data: Character):
+    characters = iniciar_conexion()
+    personaje_encontrado = characters.find_one({"_id": id_personaje})
+    
+    personaje_encontrado = {
+        "_id": id_personaje,
+        "nombre": data.name,
+        "apellido": data.last_name,
+        "año": data.age,
+    }
+    print(f"personaje encontrado: {personaje_encontrado}")
+    
+    return {"message": "id obtenido con exito",
+            "id": id_personaje,
+            "personaje_encontrado": personaje_encontrado}
